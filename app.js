6d23624ca7b6e8ec6cd74baad602dfcc3ab14ba2
Tomek <lektor1993@wp.pl>
2018-11-21 21:22:01 +0100

    created elementary logic

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore = 0, activePlayer = 0;
+scores = [0, 0];
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+function btn() {
+}
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    if (dice !== 1) {
+        roundScore+=dice;
+        console.log(roundScore); 
+        document.querySelector('#current-'+activePlayer).textContent=roundScore;
+    }
+    else {
+        activePlayer===0 ? activePlayer=1 : activePlayer=0;
+        roundScore=0;
+
+        document.getElementById('current-0').textContent='0';
+        document.getElementById('current-1').textContent='0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+    }
+
+});
+

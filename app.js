e6d5aff7ab045e68d9b49c2c6da6ec916f6c4f9d
Tomek <lektor1993@wp.pl>
2018-12-02 23:52:56 +0100

    fixed bugs

--- app.js
+++ app.js
@@ -76,10 +76,9 @@ function init() {
     document.getElementById('current-1').textContent = '0';
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
-    document
-    document.querySelector('.player-0').classList.remove('winner');
-    document.querySelector('.player-1').classList.remove('winner');
-    document.querySelector('.player-0').classList.remove('active');
-    document.querySelector('.player-1').classList.remove('active');
-    document.querySelector('.player-0').classList.add('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
